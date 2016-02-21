2066f5b71bc53d599fedff531c076dc70397a918
Andy Andrea <andy.andrea@scimedsolutions.com>
2016-02-21 18:49:35 -0500

    Add method to Image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,14 @@
 class Image < ActiveRecord::Base
   mount_uploader :file, ImageUploader
+
   validates :file, :image_set, :filter, presence: true
 
   belongs_to :image_set, inverse_of: :images
   belongs_to :filter
+
+  def filepath
+    return '' if file.blank?
+
+    file.file.file
+  end
 end
